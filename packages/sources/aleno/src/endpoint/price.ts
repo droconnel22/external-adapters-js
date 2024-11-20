b3728988ca7b545364fc8414278b811d940ec9b4
Subarna-Singh <subarnasingh11@gmail.com>
2024-11-20 13:15:05 +0530

    Changeset added; Changes as per review

--- packages/sources/aleno/src/endpoint/price.ts
+++ packages/sources/aleno/src/endpoint/price.ts
@@ -3,12 +3,10 @@ import { InputParameters } from '@chainlink/external-adapter-framework/validatio
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
 import { TransportRoutes } from '@chainlink/external-adapter-framework/transports'
 import { config } from '../config'
-import overrides from '../config/overrides.json'
 
 import { httpTransport } from '../transport/price-http'
 // import { customSubscriptionTransport } from '../transport/price-custombg'
 
-// // Input parameters define the structure of the request expected by the endpoint. The second parameter defines example input data that will be used in EA readme
 export const inputParameters = new InputParameters(
   {
     base: {
@@ -40,16 +38,10 @@ export type BaseEndpointTypes = {
 }
 
 export const endpoint = new AdapterEndpoint({
-  // Endpoint name
   name: 'price',
-  // Alternative endpoint names for this endpoint
   aliases: ['crypto', 'state'],
-  // Transport handles incoming requests, data processing and communication for this endpoint.
-  // In case endpoint supports multiple transports (i.e. http and websocket) TransportRoutes is used to register all supported transports.
-  // To use specific transport, provide `transport: [transportName]` in the request
   transportRoutes: new TransportRoutes<BaseEndpointTypes>()
     // .register('rest', httpTransport)  .register('custombg', customSubscriptionTransport),
     .register('rest', httpTransport),
   inputParameters,
-  overrides: overrides['aleno'],
 })
--- packages/sources/aleno/src/transport/price-http.ts
+++ packages/sources/aleno/src/transport/price-http.ts
@@ -19,21 +19,14 @@ export interface ResponseSchema {
   }
 }
 
-// HttpTransport extends base types from endpoint and adds additional, Provider-specific types like 'RequestBody', which is the type of
-// request body (not the request to adapter, but the request that adapter sends to Data Provider), and 'ResponseBody' which is
-// the type of raw response from Data Provider
 export type HttpTransportTypes = BaseEndpointTypes & {
   Provider: {
     RequestBody: never
     ResponseBody: ResponseSchema
   }
 }
-// HttpTransport is used to fetch and process data from a Provider using HTTP(S) protocol. It usually needs two methods
-// `prepareRequests` and `parseResponse`
+
 export const httpTransport = new HttpTransport<HttpTransportTypes>({
-  // `prepareRequests` method receives request payloads sent to associated endpoint alongside adapter config(environment variables)
-  // and should return 'request information' to the Data Provider. Use this method to construct one or many requests, and the framework
-  // will send them to Data Provider
   prepareRequests: (params, config) => {
     return params.map((param) => {
       return {
@@ -48,11 +41,8 @@ export const httpTransport = new HttpTransport<HttpTransportTypes>({
       }
     })
   },
-  // `parseResponse` takes the 'params' specified in the `prepareRequests` and the 'response' from Data Provider and should return
-  // an array of response objects to be stored in cache. Use this method to construct a list of response objects for every parameter in 'params'
-  // and the framework will save them in cache and return to user
+
   parseResponse: (params, response) => {
-    // In case error was received, it's a good practice to return meaningful information to user
     if (!response.data) {
       return params.map((param) => {
         return {
@@ -65,7 +55,6 @@ export const httpTransport = new HttpTransport<HttpTransportTypes>({
       })
     }
 
-    // For successful responses for each 'param' a new response object is created and returned as an array
     return params.map((param) => {
       let result = 0
 
